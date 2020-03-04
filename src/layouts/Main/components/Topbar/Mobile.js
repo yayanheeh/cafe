88507d5c87afb7e1a57022c611888eaa88acc191
yayanheeh <tokoxp58@gmail.com>
2020-03-04 19:10:08 +0700

    Update Mobile.js

--- src/layouts/Main/components/Topbar/Mobile.js
+++ src/layouts/Main/components/Topbar/Mobile.js
@@ -7,6 +7,13 @@ import { AppBar, Toolbar, Badge, Hidden, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
+or if your css
+
+import './home.css';
+
+
+
+
 
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
@@ -43,8 +50,10 @@ const Topbar = props => {
   const [notifications] = useState([]);
 
   return (
-    <AppBar
-      {...rest}
+
+
+    <AppBar title="My AppBar" style={{ backgroundColor: '#2196F3' }} 
+   {...rest}
       className={clsx(classes.root, className)}
     >
 
