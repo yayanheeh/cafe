19f7bfc6767938ec288fd8472e40c67802352add
yayanheeh <tokoxp58@gmail.com>
2020-03-04 19:13:54 +0700

    Update Mobile.js

--- src/layouts/Main/components/Topbar/Mobile.js
+++ src/layouts/Main/components/Topbar/Mobile.js
@@ -7,9 +7,9 @@ import { AppBar, Toolbar, Badge, Hidden, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
-or if your css
 
-import './home.css';
+
+
 
 
 
