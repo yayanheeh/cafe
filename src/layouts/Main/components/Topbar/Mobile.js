84f459e9516bd83266842fad5ca56523406af503
yayanheeh <tokoxp58@gmail.com>
2020-03-04 18:49:08 +0700

    Create Mobile.js

---
+++ src/layouts/Main/components/Topbar/Mobile.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { AppBar, Toolbar, Badge, Hidden, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
+import InputIcon from '@material-ui/icons/Input';
+
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import SearchIcon from '@material-ui/icons/Search';
+import DirectionsIcon from '@material-ui/icons/Directions';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
+
+
+const Topbar = props => {
+  const { className, onSidebarOpen, ...rest } = props;
+
+  const classes = useStyles();
+
+  const [notifications] = useState([]);
+
+  return (
+    <AppBar
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+
+<Paper component="form" className={classes.root}>
+<IconButton
+            color="inherit"
+            onClick={onSidebarOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+      <InputBase
+        className={classes.input}
+        placeholder="Search Google Maps"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+      <IconButton type="submit" className={classes.iconButton} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      <Divider className={classes.divider} orientation="vertical" />
+      <IconButton color="primary" className={classes.iconButton} aria-label="directions">
+        <DirectionsIcon />
+      </IconButton>
+    </Paper>
+      
+    </AppBar>
+  );
+};
+
+Topbar.propTypes = {
+  className: PropTypes.string,
+  onSidebarOpen: PropTypes.func
+};
+
+export default Topbar;
