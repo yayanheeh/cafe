97bc2366b77af6c79a372d759b7b75ef6e49191a
yayanheeh <tokoxp58@gmail.com>
2020-03-04 18:28:18 +0700

    Update Topbar.js

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -8,35 +8,16 @@ import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
 
-
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
-import SearchIcon from '@material-ui/icons/Search';
-import DirectionsIcon from '@material-ui/icons/Directions';
-
-
-
-
-
 const useStyles = makeStyles(theme => ({
-  root: {  padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    
+  root: {
+    boxShadow: 'none'
   },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
+  flexGrow: {
+    flexGrow: 1
   },
-  iconButton: {
-    padding: 10,
-  },
-  divider: {
-    height: 28,
-    margin: 4,
-},
-  
+  signOutButton: {
+    marginLeft: theme.spacing(1)
+  }
 }));
 
 const Topbar = props => {
@@ -46,7 +27,7 @@ const Topbar = props => {
 
   const [notifications] = useState([]);
 
-  return (<Hidden smDown>
+  return (
     <AppBar
       {...rest}
       className={clsx(classes.root, className)}
@@ -86,38 +67,6 @@ const Topbar = props => {
         </Hidden>
       </Toolbar>
     </AppBar>
-
-</Hidden>
-<Hidden smUp>
-
-    <AppBar
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-
-<Paper component="form" className={classes.root}>
-      <IconButton
-            color="inherit"
-            onClick={onSidebarOpen}
-          >
-            <MenuIcon />
-          </IconButton>
-      <InputBase
-        className={classes.input}
-        placeholder="Search Google Maps"
-        inputProps={{ 'aria-label': 'search google maps' }}
-      />
-      <IconButton type="submit" className={classes.iconButton} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-      <Divider className={classes.divider} orientation="vertical" />
-      <IconButton color="primary" className={classes.iconButton} aria-label="directions">
-        <DirectionsIcon />
-      </IconButton>
-    </Paper>
-</AppBar>
-
-</Hidden>
   );
 };
 
