a684acf3162633a05bbb13e4308fe3faefb163cb
yayanheeh <tokoxp58@gmail.com>
2020-03-05 09:25:22 +0700

    Update Budget.js

--- src/views/Dashboard/components/Budget/Budget.js
+++ src/views/Dashboard/components/Budget/Budget.js
@@ -8,52 +8,37 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
-import InfoIcon from '@material-ui/icons/Info';
-
-
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
   },
-  media: {
-    height: 140,
-  },
 });
 
-export default function MediaCard() {
+export default function ImgMediaCard() {
   const classes = useStyles();
 
   return (
     <Card className={classes.root}>
-
-          
-            <CardMedia
-          className={classes.media}
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
           image="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_1280.jpg "
           title="Contemplative Reptile"
         />
-        <GridListTile >
-
-     <GridListTileBar
-             
-              subtitle={<span>by: kres</span>}
-              actionIcon={
-                <IconButton className={classes.icon}>
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
-          </GridListTile>
-
-     
+        <CardContent>
+          
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards      </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Rp 20000
+        </Button>
         
-     
-      
+      </CardActions>
     </Card>
   );
 }
