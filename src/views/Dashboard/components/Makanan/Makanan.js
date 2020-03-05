7b74a247676d565d9eb1285b9cf5d630a5c211a9
yayanheeh <tokoxp58@gmail.com>
2020-03-05 17:09:14 +0700

    Update Makanan.js

--- src/views/Dashboard/components/Makanan/Makanan.js
+++ src/views/Dashboard/components/Makanan/Makanan.js
@@ -8,6 +8,13 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { Grid } from '@material-ui/core';    
    
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+
 
 
 function TabPanel(props) {
@@ -42,6 +49,11 @@ function a11yProps(index) {
 
 const useStyles = makeStyles(theme => ({
   root: {
+maxWidth: 345,
+  },
+  media: {
+    height: 140,
+},
     flexGrow: 1,
     width: '100%',
     backgroundColor: theme.palette.background.paper,
