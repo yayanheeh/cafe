6945d3254c1950120ac92b63aef5f387cc6ec9c4
yayanheeh <tokoxp58@gmail.com>
2020-03-05 08:40:48 +0700

    Update Mobile.js

--- src/layouts/Main/components/Topbar/Mobile.js
+++ src/layouts/Main/components/Topbar/Mobile.js
@@ -26,7 +26,7 @@ const useStyles = makeStyles(theme => ({
     padding: '2px 4px',
     display: 'flex',
     alignItems: 'center',
-    width: 300,
+    width: 350,
   },
   input: {
     marginLeft: theme.spacing(1),
