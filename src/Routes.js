d485402dfa638e37995ca3e8af66b3e2e1e91c46
yayanheeh <tokoxp58@gmail.com>
2020-03-05 10:50:23 +0700

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -14,7 +14,7 @@ import {
   Settings as SettingsView,
   SignUp as SignUpView,
   SignIn as SignInView,
-Single as SingleView,
+  Single as SingleView,
 
   NotFound as NotFoundView
 } from './views';
@@ -81,7 +81,7 @@ const Routes = () => {
         layout={MinimalLayout}
         path="/sign-in"
       />
-<RouteWithLayout
+      <RouteWithLayout
         component={Single}
         exact
         layout={MinimalLayout}
