3cc55d55e3ebbab2136713f7a3fe4f36da697d0e
yayanheeh <tokoxp58@gmail.com>
2020-03-05 11:24:54 +0700

    Update index.js

--- src/views/index.js
+++ src/views/index.js
@@ -8,4 +8,3 @@ export { default as SignIn } from './SignIn';
 export { default as SignUp } from './SignUp';
 export { default as Typography } from './Typography';
 export { default as UserList } from './UserList';
-export { default as Single } from './Single';
