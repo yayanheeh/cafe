afd88f12ff5a84e430809fb6729efc98fffb9e53
yayanheeh <tokoxp58@gmail.com>
2020-03-05 16:46:03 +0700

    Update Dashboard.js

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -6,16 +6,6 @@ import Hidden from '@material-ui/core/Hidden';
 
 
 
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-
-
-
-
 import {
   Budget,
   TotalUsers,
@@ -40,34 +30,6 @@ const Dashboard = () => {
 
   return (
    <div className={classes.root}>
-<Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-        >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-        </Tabs>
-      
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-        <TabPanel value={value} index={0} dir={theme.direction}>
-          Item One
-        </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-          Item Three
-        </TabPanel>
-      </SwipeableViews>
 
 
 
@@ -83,8 +45,7 @@ const Dashboard = () => {
 
       <Hidden smDown>
         <Grid
-          item
-          lg={3}
+               lg={3}
           sm={6}
           xl={3}
           xs={12}
