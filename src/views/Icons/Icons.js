12de3a2903f5788a4a4c89ca911ad5c6065fb3d4
yayanheeh <tokoxp58@gmail.com>
2020-03-06 14:54:17 +0700

    Update Icons.js

--- src/views/Icons/Icons.js
+++ src/views/Icons/Icons.js
@@ -12,10 +12,10 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 350,
+    maxWidth: 200,
   },
   media: {
-    height: 140,
+    height: 90,
   },
 });
 
@@ -61,7 +61,27 @@ export default function FullWidthGrid() {
             Makan    </Typography>
         </CardContent>     </CardActionArea>   </Card>
         
-      </Grid></Grid>
+      </Grid>
+
+
+<Grid item xs={6} sm={3}>
+      
+<Card className={classes.root}>
+ <CardActionArea>  
+
+ <CardMedia
+          className={classes.media}
+          image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          
+          <Typography variant="body2" color="textSecondary" component="p">
+            Makan    </Typography>
+        </CardContent>     </CardActionArea>   </Card>
+        
+      </Grid>
+</Grid>
     </div>
   );
 }
