dd6d929a2263e1e55af9b8a6f8c76ad0548c85b8
yayanheeh <tokoxp58@gmail.com>
2020-03-06 13:14:53 +0700

    Update Icons.js

--- src/views/Icons/Icons.js
+++ src/views/Icons/Icons.js
@@ -1,47 +1,41 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-import React from "react";
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 200,
+  },
+});
 
-// @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
-import withWidth from '@material-ui/core/withWidth';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
+export default function ImgMediaCard() {
+  const classes = useStyles();
 
-const styles = theme => ({ });
-
-class Icons extends React.Component {
-    render() {
-        const { classes, currentUser, images, width } = this.props;
-
-        let columns = width === 'xs' || width === 'sm'  ? 1 : 2;
-
-        return (
-            <div className={classes.root}>
-                <GridList cellHeight={180} cols={columns} className={classes.gridList}>
-                    Tes
-                   
-                </GridList>
-            </div>
-        );
-    }
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Spageti </Typography>
+        </CardContent>
+      </CardActionArea>
+      
+    </Card>
+  );
 }
-
-Icons.propTypes = {
-    currentUser: PropTypes.object,
-    images: PropTypes.array.isRequired
-};
-
-function mapStateToProps(state) {
-    return {
-        currentUser: state.user.currentUser
-    };
-}
-
-export default compose(
-  withStyles(styles, {
-    name: 'Icons',
-  }),
-  withWidth(),
-  connect(mapStateToProps),
-)(Icons);
