436a2b90288f2d81a2eae67cf66ba5091a79f4e7
yayanheeh <tokoxp58@gmail.com>
2020-03-06 09:28:31 +0700

    Update Icons.js

--- src/views/Icons/Icons.js
+++ src/views/Icons/Icons.js
@@ -14,13 +14,24 @@ const useStyles = makeStyles({
   },
 });
 
-function Icons(props) {
-  const { classes } = props;
-  const bull = <span className={classes.bullet}>•</span>;
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function SimpleTable() {
+  const classes = useStyles();
 
   return (
-   
- <Table className={classes.table} aria-label="simple table">
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Dessert (100g serving)</TableCell>
@@ -30,12 +41,20 @@ function Icons(props) {
             <TableCell align="right">Protein&nbsp;(g)</TableCell>
           </TableRow>
         </TableHead>
-    </Table>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
-
-Icons.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles, { withTheme: true })(Icons);
