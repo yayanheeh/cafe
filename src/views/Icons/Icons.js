3992240079e8f02e9f34d7eea55ba53cec0463e6
yayanheeh <tokoxp58@gmail.com>
2020-03-06 14:05:35 +0700

    Update Icons.js

--- src/views/Icons/Icons.js
+++ src/views/Icons/Icons.js
@@ -1,74 +1,33 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import { Grid } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-        
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-flexGrow: 1,
+    flexGrow: 1,
   },
-
-    maxWidth: 200,
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
   },
-});
+}));
 
-
-export default function {ImgMediaCard,FullWidthGrid}() {
+export default function FullWidthGrid() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-     <Grid item xs={6} sm={3}>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          
-          <Typography variant="body2" color="textSecondary" component="p">
-            Spageti </Typography>
-        </CardContent>
-     
-</Grid>
-
-<Grid item xs={6} sm={3}>
-
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          
-          <Typography variant="body2" color="textSecondary" component="p">
-            Spageti </Typography>
-        </CardContent>
-     
-</Grid>
-
- 
-<Grid item xs={6} sm={3}>
-          1        </Grid>
+      <Grid container spacing={1}>
+        
         <Grid item xs={6} sm={3}>
-          2        </Grid>
+          1</Grid>
         <Grid item xs={6} sm={3}>
-          3        </Grid>
+          2    </Grid>
         <Grid item xs={6} sm={3}>
-          4        </Grid>
-
-      
+          3    </Grid>
+      </Grid>
     </div>
   );
 }
