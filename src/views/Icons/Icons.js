cb839e810a19e9879d0bf6cd6255fde1d1158609
yayanheeh <tokoxp58@gmail.com>
2020-03-06 13:34:02 +0700

    Update Icons.js

--- src/views/Icons/Icons.js
+++ src/views/Icons/Icons.js
@@ -7,7 +7,9 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Grid } from '@material-ui/core';
 
+        
 const useStyles = makeStyles({
   root: {
     maxWidth: 200,
@@ -19,21 +21,12 @@ export default function ImgMediaCard() {
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          
-          <Typography variant="body2" color="textSecondary" component="p">
-            Spageti </Typography>
-        </CardContent>
-      </CardActionArea>
-
+     <Grid
+               lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
 <CardActionArea>
         <CardMedia
           component="img"
@@ -48,6 +41,33 @@ export default function ImgMediaCard() {
             Spageti </Typography>
         </CardContent>
       </CardActionArea>
+</Grid>
+
+<Grid
+               lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+<CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          
+          <Typography variant="body2" color="textSecondary" component="p">
+            Spageti </Typography>
+        </CardContent>
+      </CardActionArea>
+</Grid>
+
+ 
+
+
       
     </Card>
   );
