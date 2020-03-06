6e76a52498ad30fba95556f5e33403209a58bdd0
yayanheeh <tokoxp58@gmail.com>
2020-03-06 13:17:43 +0700

    Update Icons.js

--- src/views/Icons/Icons.js
+++ src/views/Icons/Icons.js
@@ -28,9 +28,22 @@ export default function ImgMediaCard() {
           title="Contemplative Reptile"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
+          
+          <Typography variant="body2" color="textSecondary" component="p">
+            Spageti </Typography>
+        </CardContent>
+      </CardActionArea>
+
+<CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          
           <Typography variant="body2" color="textSecondary" component="p">
             Spageti </Typography>
         </CardContent>
