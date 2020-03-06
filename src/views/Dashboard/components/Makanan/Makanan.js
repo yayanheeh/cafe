2c4e749ed120a26697a88a1afee5a276d4e7a858
yayanheeh <tokoxp58@gmail.com>
2020-03-06 12:10:13 +0700

    Update Makanan.js

--- src/views/Dashboard/components/Makanan/Makanan.js
+++ src/views/Dashboard/components/Makanan/Makanan.js
@@ -15,6 +15,13 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
 
 
 function TabPanel(props) {
